abdae86e06f64e60b94cc04b68f6513788c1d151
adineharshiya <adineharshiya@gmail.com>
2026-06-29 20:19:05 +0330

    fix basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/Hotel-reservation',  // <-- این خط رو اضافه کن
+  basePath: '/Hotel',  // <-- این خط مهمه
   images: {
     unoptimized: true,
     remotePatterns: [
